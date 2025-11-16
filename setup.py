fa71a21b7441ead92ac773cecd42b1bbbde9f83a
SSankit <125234292+SSankit@users.noreply.github.com>
2025-11-16 01:02:27 +0000

    initial clean version of college app

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="college",
+    version="0.0.1",
+    description="College management system",
+    author="IDML",
+    author_email="ankit@idesignmylife.net",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False
+)
